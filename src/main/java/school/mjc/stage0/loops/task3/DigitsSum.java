d07bc033a822334e966f94e0e228bcdfbd6ccab7
Mirabbos <mirabbosahmedov7777@gmail.com>
2024-04-23 22:50:53 +0500

    6 functions completed

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -2,6 +2,12 @@ package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
     public void printDigitsSum(int t){
-
+        int a = t;
+        int res = 0;
+        for (int i = 0; i < Integer.toString(a).length(); i++) {
+            res += t % 10;
+            t = t / 10;
+        }
+        System.out.println(Math.abs(res));
     }
 }
--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,6 +2,15 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
+        long sum = 0;
+        long term = 0;
+        long multiplier = 1;
 
+        for (int i = 1; i <= lengthOfLastNumber; i++) {
+            term = term * 10 + 9;
+            sum += term;
+        }
+
+        System.out.println(sum);
     }
 }
